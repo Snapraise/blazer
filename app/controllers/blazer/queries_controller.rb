785e72adce5dc3e88649f9194151cb0379b18cc8
jason <jason.mattingly@hotmail.com>
2017-11-09 14:20:28 -0800

    department now shows for both query and dashboards

--- app/controllers/blazer/queries_controller.rb
+++ app/controllers/blazer/queries_controller.rb
@@ -30,7 +30,7 @@ module Blazer
             name: d.name,
             creator: blazer_user && d.try(:creator) == blazer_user ? "You" : d.try(:creator).try(Blazer.user_name),
             to_param: d.to_param,
-            blazer_group: (BlazerGroup.find(d.blazer_group_id).name).partition('_').last.titleize,
+            blazer_group: BlazerGroup.find(d.blazer_group_id).name.partition('_').last.titleize,
             dashboard: true
           }
         end
@@ -288,6 +288,7 @@ module Blazer
               name: q.name,
               creator: blazer_user && q.try(:creator) == blazer_user ? "You" : q.try(:creator).try(Blazer.user_name),
               vars: q.variables.join(", "),
+              blazer_group: BlazerGroup.find(q.blazer_group_id).name.partition('_').last.titleize,
               to_param: q.to_param
             }
           end
